07f81e3feeccf4d49bff1a79fa9991c9aaba6c0e
Amir Nourinia <amirnourinia@Amirs-MacBook-Pro.local>
2020-04-01 16:21:58 +0200

    comments

--- Lec1/main.py
+++ Lec1/main.py
@@ -3,6 +3,7 @@ import numpy as np
 numberOfDays = 20
 model = np.array([[.5, .5, .25], [.25, 0., .25], [.25,.5,.5]])
 
+# Reshaping to avoid zero order vectors.
 today = np.array([1,0,0]).reshape(3,1)
 
 for i in range(numberOfDays):
