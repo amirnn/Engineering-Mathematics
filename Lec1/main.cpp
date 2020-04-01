c438276079f7931aca8f8e5d3f672a86e95d892c
Amir Nourinia <amirnourinia@Amirs-MacBook-Pro.local>
2020-04-01 15:01:05 +0200

    Lecture1 example

--- Lec1/main.cpp
+++ Lec1/main.cpp
@@ -3,7 +3,23 @@
 
 
 int main(int argc, char const *argv[])
-{
-    
+{    
+    unsigned numberOfDays{20};
+    Eigen::Matrix3f model;
+    model << .5, .5, .25,
+            .25, 0, .25,
+            .25, .5, .5;
+    std::cout << "The model matrix is:\n" << model << std::endl;
+
+    Eigen::Vector3f today;
+    Eigen::Vector3f tommorow;
+    today << 1, 0 ,0;
+
+    for (unsigned i=0; i<numberOfDays ;i++){
+        std::cout << std::endl << "Day "<< i << std::endl << "Today's Weather: " << std::endl << today << std::endl;
+                tommorow = model * today;
+                today = tommorow;
+    }
+
     return 0;
 }
