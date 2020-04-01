8ae7967b264abd4c396a689d0294b112ba45498b
Amir Nourinia <amirnourinia@Amirs-MacBook-Pro.local>
2020-04-01 16:16:19 +0200

    Added the python program.

---
+++ Lec1/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+numberOfDays = 20
+model = np.array([[.5, .5, .25], [.25, 0., .25], [.25,.5,.5]])
+
+today = np.array([1,0,0]).reshape(3,1)
+
+for i in range(numberOfDays):
+    print("Day {0}:\nToday's weather:\n{1}\n".format(i,today))
+    tomorrow = model.dot(today) 
+    today = tomorrow
+
+# print(today.shape)
