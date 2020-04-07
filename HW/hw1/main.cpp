2ee1dfe5e44ba2e0aa308f8ec33564432e12e442
Amir Nourinia <amirnourinia@Amirs-MacBook-Pro.local>
2020-04-07 23:24:19 +0200

    odient for 2nd order system.

--- HW/hw1/main.cpp
+++ HW/hw1/main.cpp
@@ -2,12 +2,39 @@
 #include <Eigen/Dense>
 #include <vector>
 #include <cmath>
+#include <boost/numeric/odeint.hpp>
 #include "gnuplot-iostream.h"
+#include "system.h"
+namespace odeint = boost::numeric::odeint;
+int main()
+{
+	state_type x(2); // a vector with size 2.
+	x[0] = 1.0;		 // start at x=1.0, p=0.0
+	x[1] = 0.0;
+	std::vector<state_type> x_vec;
+	std::vector<double> times;
 
-int main() {
+	size_t steps = odeint::integrate(harmonic_oscillator,
+									 x, 0.0, 10.0, 0.1,
+									 push_back_state_and_time(x_vec, times));
 
-//
 
-return 0;
+	std::vector<double> position;
+	for(size_t i=0; i<=steps;i++){
+		position.push_back(x_vec[i][0]);
+	}
+	/* output */
+	Gnuplot gp;
+	gp << "plot '-' using 1:2 with linespoint" << std::endl;
+	gp.send1d(std::make_tuple(times,position));
 
+
+
+
+	for (size_t i = 0; i <= steps; i++)
+	{
+		std::cout << times[i] << '\t' << x_vec[i][0] << '\t' << x_vec[i][1] << '\n';
+	}
+
+	return 0;
 }
