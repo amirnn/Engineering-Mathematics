78db9f284180bcdc75151b51b9797eea63f988a1
Amir Nourinia <amirnourinia@Amirs-MacBook-Pro.local>
2020-04-14 23:21:37 +0200

    Eigen vector calculation for Exercise 2

--- HW/hw2/main.cpp
+++ HW/hw2/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <Eigen/Dense>
-
 #include "gnuplot-iostream.h"
 
+using Eigen::Matrix3d;
+using Eigen::Matrix3cd;
 int main(int argc, char const *argv[])
 {
-    /* code */
+    Matrix3d A;
+    A <<    0,1,0,
+            0,0,1,
+            2,1,-2;
+    std::cout << "Here is the matrix A:\n" << A << std::endl;
+
+    Eigen::EigenSolver<Matrix3d> eigenSolver(A);
+    if(eigenSolver.info() != Eigen::Success) abort();
+    std::cout << "The eigenvalues of A are:\n" << eigenSolver.eigenvalues() << std::endl;
+    std::cout << "Here's a matrix whose columns are eigenvectors of A \n"
+        << "corresponding to these eigenvalues:\n"
+        << eigenSolver.eigenvectors() << std::endl;
+
+    Matrix3cd T = eigenSolver.eigenvectors();
+    Matrix3cd T_inverse = T.inverse();
+    Matrix3cd D = T_inverse * A * T;
+
+    std::cout << "The diagonlized Matrix D is:\n" << D << std::endl;
+
+
     return 0;
 }
