d5d777ff271d51d9d1a4376fc81bc23cb2649a37
Amir Nourinia <amirnourinia@Amirs-MacBook-Pro.local>
2020-04-16 17:31:10 +0200

    added readme.

--- HW/hw2/exercise2_3.cpp
+++ HW/hw2/exercise2_3.cpp
@@ -5,30 +5,21 @@
 typedef arma::mat::fixed<4, 4> SystemMatrix;
 int main(int argc, char const *argv[])
 {
-    // arma::cx_vec eigval;
-    // arma::cx_mat eigvec;
-    // SystemMatrix sysMatrix{
-    // {0,1,0,0},
-    // {0,0,1,0},
-    // {0,0,0,1},
-    // {0,0,0,0}
-    // };
-    // auto ebsVec = arma::linspace(0, .05, 100);
-    // for (const auto& ebs: ebsVec){
-    //     sysMatrix(3,0) = -system_function::alpha_thetha_func(ebs);
-    //     sysMatrix(3,2) = -system_function::alpha_thetha_dd_func(ebs);
-    //     arma::eig_gen( eigval, eigvec, sysMatrix, "balance");
-    //     //std::cout << "Here are the eivalues for the ebs: " << ebs << std::endl << eigval << std::endl;
-    // }
-
-    {
-        arma::mat A = arma::randu<arma::mat>(10, 10);
-
-        arma::cx_vec eigval;
-        arma::cx_mat eigvec;
-
-        arma::eig_gen(eigval, eigvec, A);
-        arma::eig_gen(eigval, eigvec, A, "balance");
+    arma::cx_vec eigval;
+    arma::cx_mat eigvec;
+    SystemMatrix sysMatrix{
+    {0,1,0,0},
+    {0,0,1,0},
+    {0,0,0,1},
+    {0,0,0,0}
+    };
+    auto ebsVec = arma::linspace(0, .05, 100);
+    for (const auto& ebs: ebsVec){
+        sysMatrix(3,0) = -system_function::alpha_thetha_func(ebs);
+        sysMatrix(3,2) = -system_function::alpha_thetha_dd_func(ebs);
+        arma::eig_gen( eigval, eigvec, sysMatrix, "balance");
+        std::cout << "Here are the eigvalues for the ebs: " << ebs << std::endl << eigval << std::endl;
+        std::cout << "Here are the eigvectors: " << std::endl << eigvec << std::endl;
     }
 
     return 0;
---
+++ HW/hw2/readme.txt
@@ -0,0 +1,3 @@
+To solve linking issues in exercise2_3 we have to comment out the line #define ARMA_USE_WRAPPER in config.h in include folder of the
+Armadillo.
+on macos I'm linking agains lapack blas. Which can be dound in brew under the names lapack and openblas.
