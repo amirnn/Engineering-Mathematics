8192a4c82982f74d8072c71a6fe329d6341dbf7a
Amir Nourinia <amirnourinia@Amirs-MacBook-Pro.local>
2020-04-16 18:57:29 +0200

    added try/catch block for the eigen decomosition.

--- HW/hw2/exercise2_3.cpp
+++ HW/hw2/exercise2_3.cpp
@@ -5,21 +5,30 @@
 typedef arma::mat::fixed<4, 4> SystemMatrix;
 int main(int argc, char const *argv[])
 {
-    arma::cx_vec eigval;
-    arma::cx_mat eigvec;
-    SystemMatrix sysMatrix{
-    {0,1,0,0},
-    {0,0,1,0},
-    {0,0,0,1},
-    {0,0,0,0}
-    };
-    auto ebsVec = arma::linspace(0, .05, 100);
-    for (const auto& ebs: ebsVec){
-        sysMatrix(3,0) = -system_function::alpha_thetha_func(ebs);
-        sysMatrix(3,2) = -system_function::alpha_thetha_dd_func(ebs);
-        arma::eig_gen( eigval, eigvec, sysMatrix, "balance");
-        std::cout << "Here are the eigvalues for the ebs: " << ebs << std::endl << eigval << std::endl;
-        std::cout << "Here are the eigvectors: " << std::endl << eigvec << std::endl;
+    try
+    {
+        arma::cx_vec eigval;
+        arma::cx_mat eigvec;
+        SystemMatrix sysMatrix{
+            {0, 1, 0, 0},
+            {0, 0, 1, 0},
+            {0, 0, 0, 1},
+            {0, 0, 0, 0}};
+        auto ebsVec = arma::linspace(0, .05, 100);
+        for (const auto &ebs : ebsVec)
+        {
+            sysMatrix(3, 0) = -system_function::alpha_thetha_func(ebs);
+            sysMatrix(3, 2) = -system_function::alpha_thetha_dd_func(ebs);
+            arma::eig_gen(eigval, eigvec, sysMatrix, "balance");
+            std::cout << "Here are the eigvalues for the ebs: " << ebs << std::endl
+                      << std::setprecision(2) << eigval << std::endl;
+            std::cout << "Here are the eigvectors: " << std::endl
+                      << std::setprecision(2) << eigvec << std::endl;
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
     }
 
     return 0;
